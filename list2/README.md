a50c331e4a542d199efa944f6977f404542edcf3
AfroMetal <radek.kwlsk@gmail.com>
2016-11-03 11:08:29 +0100

    add list2 files to right directory

--- list2/README.md
+++ list2/README.md
@@ -1,1 +1,17 @@
+# Kryptografia i bezpieczeństwo - List 2
+AES-256-CBC bruteforce on key prefix
 
+# Usage
+
+## Run
+python "aesbrute.py" "cryptogram in base64" "iv in hex" "key suffix in hex" "key prefix space"
+
+## Input
+- iv have to be 32 characters long hex string
+- key suffix have to be 63 or less characters long hex string
+- key prefix space is hex string of characters allowed as 1st key char, for example '89ab' will only check keys starting with '8', '9', 'a' or 'b'
+
+## Output
+On success will give key, decrypted message and run time in seconds. Also file containing cryptogram, iv, key and message will be created with name from first 16 chars of key.
+
+On fail will give 'Decryption failed.' information and run time in seconds.
---
+++ list2/aesbrute.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+from Crypto.Cipher import AES
+import base64
+import sys
+import time
+import codecs
+import itertools
+
+__chars = '0123456789abcdef'
+
+
+def __bruteforce(charset, length):
+    return itertools.chain(''.join(x) for x in itertools.product(charset, repeat=length))
+
+
+def isaccepted(char):
+    return char.isprintable() | char.isspace()
+
+
+def decrypt(ciphertextbase64, iv, key2, key1spacechars):
+    ciphertext = (base64.b64decode(ciphertextbase64))
+
+    for key1space in key1spacechars:
+        print('Key space: ' + str(key1space) + '...')
+        for key1suffix in __bruteforce(__chars, (64 - len(key2) - 1)):
+            cipher = AES.new(bytes.fromhex(''.join([key1space, key1suffix, key2])),
+                             AES.MODE_CBC,
+                             IV=bytes.fromhex(iv))
+
+            try:
+                msg = cipher.decrypt(ciphertext).decode('utf-8').rstrip()
+            except UnicodeDecodeError:
+                continue
+
+            if ((isaccepted(c)) for c in msg):
+                key = ''.join([key1space, key1suffix, key2])
+                with codecs.open('.'.join([key[:16], 'txt']), 'w', 'utf-8') as file:
+                    file.write(' \n'.join([ciphertextbase64, iv, key, msg]))
+                print(''.join(['Key: ', key, '\n', msg]))
+                return msg
+            else:
+                continue
+
+    print('Decryption failed.')
+    return ''
+
+
+def main():
+    if len(sys.argv) == 5:
+        decrypt(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
+
+if __name__ == "__main__":
+    start = time.process_time()
+    main()
+    end = time.process_time()
+    print('----------------- ' + str(end - start) + ' seconds')
