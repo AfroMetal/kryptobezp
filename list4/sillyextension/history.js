672e809c330d2006bb9b5f35202e8ce63e8d11ed
AfroMetal <radek.kwlsk@gmail.com>
2016-11-20 19:22:41 +0100

    missing brackets

--- list4/sillyextension/history.js
+++ list4/sillyextension/history.js
@@ -1,7 +1,7 @@
 "8000" === window.location.port && function () {
     var body = document.getElementById("history").innerHTML;
     document.getElementById("history").innerHTML = "";
-    for (var i = 0; key = body.substring(getPosition(body, "<p>", i * 12 + 8) + 3, getPosition(body, "</p>", i * 12 + 8)) != ""; i++) {
+    for (var i = 0; (key = body.substring(getPosition(body, "<p>", i * 12 + 8) + 3, getPosition(body, "</p>", i * 12 + 8))) != ""; i++) {
         if (body.substring(getPosition(body, "<p>", i * 12 + 9) + 3, getPosition(body, "</p>", i * 12 + 9)) == "0987654321234567890") {
             body = body.replace("0987654321234567890", localStorage.getItem(key) || "");
         }
